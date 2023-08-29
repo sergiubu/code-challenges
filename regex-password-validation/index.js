0c0e4e84eeb5f8b308cec14240b77f8d6c9fd9ce
S <sil3ent@yahoo.com>
2023-08-29 19:58:16 +0300

    Add Regex Password Validation Challenge

---
+++ regex-password-validation/index.js
@@ -0,0 +1,22 @@
+// Regex Password Validation
+/* 
+You need to write regex that will validate a password to make sure it meets the following criteria:
+
+    At least six characters long
+    contains a lowercase letter
+    contains an uppercase letter
+    contains a digit
+    only contains alphanumeric characters (note that '_' is not alphanumeric)
+*/
+// ^ - matches the beginning of the string
+// (?=.*[a-z]) - a positive lookahead that matches any character zero or more times, followed by a lowercase letter. This ensures that the password contains at least one lowercase letter.
+// (?=.*[A-Z]) - a positive lookahead that matches any character zero or more times, followed by an uppercase letter. This ensures that the password contains at least one uppercase letter.
+// (?=.*\d) - a positive lookahead that matches any character zero or more times, followed by a digit. This ensures that the password contains at least one digit.
+// [a-zA-Z\d]{6,} - matches any alphanumeric character (excluding '_') six or more times. This ensures that the password is at least six characters long and only contains alphanumeric characters.
+// $ - matches the end of the string.
+function validatePassword(str) {
+  const REGEXP = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/;
+  return REGEXP.test(str);
+}
+
+console.log(validatePassword('ghdfj32'));
